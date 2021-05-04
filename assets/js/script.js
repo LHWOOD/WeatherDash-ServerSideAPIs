5b91d304f349db4020f72eb3d7125aee4242e17d
LHWOOD <hutsonwood@gmail.com>
2021-05-03 23:07:10 -0600

    created the fetch api function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,37 +1,45 @@
+//variables
 var userForm = document.querySelector("#city-search-form");
 var cityInput = document.querySelector("#city-search");
+var infoDump = document.querySelector("#dump");
 
+//captures city search
 var inputHandler = function (event) {
   event.preventDefault();
   var city = cityInput.value;
   console.log(city);
-  //needs this to text content of city weather
+  getApi();
 };
 
-//function to contact API
-// function getApi() {
-//   var requestUrl =
-//     "https://api.openweathermap.org/data/2.5/weather?q=Denver&appid=74f3ae2f827155ea451bd2e9b7cacb93";
+// function fetch API info
+function getApi(city) {
+  var city = cityInput.value;
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    city +
+    "&units=imperial&appid=74f3ae2f827155ea451bd2e9b7cacb93";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      var cityName = document.createElement("h1");
+      var temp = document.createElement("h4");
+      var windSpeed = document.createElement("h4");
+      var humidity = document.createElement("h4");
 
-//   fetch(requestUrl)
-//     .then(function (response) {
-//       return response.json();
-//     })
-//     .then(function (data) {
-//       console.log(data);
-//       //   var cold = document.createElement("h3");
+      cityName.textContent = data.name;
+      temp.textContent = "Temp: " + data.main.temp + " °F";
+      windSpeed.textContent = "Wind: " + data.wind.speed + " MPH";
+      humidity.textContent = "Humidity: " + data.main.humidity + " %";
 
-//       //   cold.textContent = data.main.humidity;
+      infoDump.appendChild(cityName);
+      infoDump.appendChild(temp);
+      infoDump.appendChild(windSpeed);
+      infoDump.appendChild(humidity);
+    });
+}
 
-//       //   infoDump.appendChild(cold);
-//     });
-// }
-
-// var displayWeather = function (weather, searchCity) {
-//   cityInput.textContent = searchCity;
-
-//   console.log(weather);
-// };
-
+//event listener for form
 userForm.addEventListener("submit", inputHandler);
-// userForm.addEventListener("submit", getApi);
